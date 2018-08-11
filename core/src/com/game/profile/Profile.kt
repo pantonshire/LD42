cd5fa22cb75a8daa08bc4b67c58e934b49accb31
Tom Panton <tom@tomandtally.co.uk>
2018-08-11 21:54:16 +0100

    Weird portrait dimensions

--- core/src/com/game/profile/Profile.kt
+++ core/src/com/game/profile/Profile.kt
@@ -8,6 +8,6 @@ class Profile(
         val greeting: String
 ) {
 
-
+    
 
 }
--- desktop/src/com/game/desktop/DesktopLauncher.java
+++ desktop/src/com/game/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ public class DesktopLauncher {
 
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 720;
+		config.height = 1280;
 		new LwjglApplication(new Main(), config);
 	}
 
