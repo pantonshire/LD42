44ca8633adcaf6c6a149c28326590b66fa147403
Tom Panton <tom@tomandtally.co.uk>
2018-08-11 22:50:12 +0100

    Face pcg foundations

--- core/src/com/game/profile/Profile.kt
+++ core/src/com/game/profile/Profile.kt
@@ -1,13 +1,25 @@
 package com.game.profile
 
+import java.util.*
+
 class Profile(
         val forename: String,
         val lastname: String,
-        val age: Int,
+        val age: String,
         val gender: String,
         val greeting: String
 ) {
 
-    
+    fun fullName(): String = "$forename $lastname"
+
+    fun ageAsInt(): Int = Integer.valueOf(age)
+
+    fun pcgFaceParts(): Array<Int> {
+        val seed = (forename + lastname).hashCode()
+        val rng = Random(seed.toLong())
+        return IntArray(6) { _ -> rng.nextInt(5) }.toTypedArray()
+    }
+
+
 
 }
